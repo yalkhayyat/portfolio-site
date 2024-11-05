c42290a95f840bb96d8ed095e91e145dc367e4fd
Yousif Alkhayyat <yalkhayy@gmail.com>
2024-11-05 04:34:57 -0600

    eslint ignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    optimizeFonts: true
+    optimizeFonts: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
